bcb9c73822d0c3d3beadc7bce07ae94ffb308642
Robert James Walters <mrrobertwalters27@gmail.com>
2025-11-29 12:41:57 -0800

    Fixes on visuals 2
    Correcting mistakes in last commit

--- shop.php
+++ shop.php
@@ -73,7 +73,7 @@ hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 <section class="filter-section">
     <div class="container">
         <form class="filter-buttons" method="GET" action="shop.php">
-            <button class="filter-btn <?php if ($category === "all" || $category === null)
+            <button class="filter-btn <?php if ($category !== "Y Series" && $category !== "Large Format")
                 echo "active"; ?>" name="category" value="all" type="submit">All</button>
             <button class="filter-btn <?php if($category === "Y Series")
                 echo "active";?>" name="category" value="Y Series" type="submit">Y Series</button>
