dd94a533e25b40a67f65192b7e1e0c635ed632cd
QuirkyTurtle99 <thaddeaus1@hotmail.com>
2025-11-28 20:26:44 -0800

    log in and out

--- logout.php
+++ logout.php
@@ -1,12 +1,18 @@
 <?php
 session_start();
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 session_destroy();
 header("Location: index.php");
 exit();
 =======
+=======
+>>>>>>> Stashed changes
 session_unset();
 session_destroy();
 header('Location: shop.php');
 exit;
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
