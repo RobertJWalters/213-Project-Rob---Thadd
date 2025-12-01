954bbdc35a6013b2dbaf0322c95082d28351881b
QuirkyTurtle99 <thaddeaus1@hotmail.com>
2025-11-30 18:15:31 -0800

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 class db {
     public static function getDB() {
-        $mysqli = new mysqli("localhost", "rute", "password", "proj");
+        $mysqli = new mysqli("localhost", "root", "ttocseerB1!", "project");
 
         if ($mysqli->connect_error) {
             die("Connection failed: " . $mysqli->connect_error);
